a28089f130a874815042b55df453fc97cef05980
sumit.vishwakarma <sumit.vishwakarma@speridian.com>
2023-11-21 00:45:26 +0530

    1.Adding string resource

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -4,11 +4,9 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -16,8 +14,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -29,6 +33,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,12 +51,12 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier= Modifier){
+fun MyApp(modifier: Modifier = Modifier) {
     var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
     Surface(modifier) {
-        if(shouldShowOnboarding){
+        if (shouldShowOnboarding) {
             OnboardingScreen(onContinueCLicked = { shouldShowOnboarding = false })
-        }else{
+        } else {
             Greetings()
         }
     }
@@ -75,14 +80,14 @@ fun OnboardingScreen(modifier: Modifier = Modifier, onContinueCLicked: () -> Uni
 }
 
 @Composable
-fun Greetings(modifier: Modifier = Modifier, name: List<String> = List(10){"$it"}) {
+fun Greetings(modifier: Modifier = Modifier, name: List<String> = List(10) { "$it" }) {
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
-        LazyColumn(modifier = modifier.padding(vertical = 4.dp)){
-            items(items = name) {
-                name -> Greeting(name = name)
+        LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
+            items(items = name) { name ->
+                Greeting(name = name)
             }
         }
 
@@ -99,11 +104,29 @@ fun OnboardingPreview() {
 
 @Composable
 fun Greeting(name: String) {
+    Card(
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.primary
+        ),
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        CardContent(name = name)
+    }
+
+}
+
+@Composable
+fun CardContent(name: String) {
     var expanded by remember {
         mutableStateOf(false)
     }
-    val extraPadding by animateDpAsState(if(expanded) 48.dp else 0.dp,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow),
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        ),
+        label = "",
     )
     Surface(
         color = MaterialTheme.colorScheme.primary,
@@ -116,17 +139,42 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello,")
-                Text(text = name, style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.ExtraBold))
-            }
-
-            ElevatedButton(
-                onClick = {
-                    expanded = !expanded
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Medium)
+                )
+                if (expanded) {
+                    Text(text = ("Compose testing with " + "padding and card content").repeat(4))
                 }
-            ) {
-                Text(if(expanded)"Show less" else "Show more")
+            }
+            IconButton(onClick = { expanded = !expanded
+            }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+                )
             }
         }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingCardPreview() {
+    BasicsCodelabTheme {
+        CardContent(name = "TestGreetingCard")
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun GreetingPreview() {
+    BasicsCodelabTheme {
+        Greeting(name = "Test")
     }
 }
 
@@ -141,18 +189,19 @@ fun DefaultPreview() {
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun MyAppPreview(){
+fun MyAppPreview() {
     BasicsCodelabTheme {
-        MyApp(Modifier.fillMaxSize( ))
+        MyApp(Modifier.fillMaxSize())
     }
 }
 
 @Preview(showBackground = true, widthDp = 320)
 @Composable
-fun GreetingsPreview(){
+fun GreetingsPreview() {
     BasicsCodelabTheme {
         Greetings()
     }
 }
+
 
 
